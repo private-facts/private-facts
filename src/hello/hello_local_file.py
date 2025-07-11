00d0e20c9a5150f33f7303bea6ea07b1f6c0d238
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-07-11 15:07:03 -0400

    Add justfile

--- src/hello/hello_local_file.py
+++ src/hello/hello_local_file.py
@@ -42,7 +42,7 @@ def get_file(tahoe_client, cap_string, output_path):
     """
     Retrieve the contents of a file by passing the capability string to the tahoe_client and write them to output_path.
     """
-    retrieved_data, status = tahoe_client.retrieve_data(cap_string)
+    retrieved_data, status = tahoe_client.get_data(cap_string)
 
     if status != 200:
         print(f"An error occurred retrieving the data with error code: {status}")
